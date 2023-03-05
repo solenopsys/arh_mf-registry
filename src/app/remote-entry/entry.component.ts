6e0133825e70a7c35bb50d20dd065530b83ab2a3
AlexStorm <info@alexstorm.ru>
2023-03-05 09:23:27 +0530

    fix names

--- src/app/remote-entry/entry.component.ts
+++ src/app/remote-entry/entry.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hyperconverged-registry-entry',
+  selector: 'sc-registry-entry',
   template: `<div class="remote-entry">
     <h2>registry's Remote Entry Component</h2>
   </div>`,
