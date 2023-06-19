0ef6228e9cddceca0aafed11edee88715ce16ea6
AlexStorm <info@alexstorm.ru>
2023-06-19 12:48:20 +0530

    refactor modules

--- src/app/entry.module.ts
+++ src/app/entry.module.ts
@@ -1,5 +1,27 @@
-import { NgModule } from '@angular/core';
-import { IMPORTS_CONF, PROVIDERS_CONF } from "./app.module";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { RouterModule, Routes } from "@angular/router";
+import { BootstrapComponent, TABLE_PAGE } from "@solenopsys/ui-templates";
+import { FormsModule } from "@angular/forms";
+import { TABLES } from "./tables.config";
+
+const routes: Routes = [
+  TABLE_PAGE(':table'),
+];
+
+export const PROVIDERS_CONF = [
+  {provide: 'tables', useValue: TABLES},
+  {provide: 'assets_dir', useValue: "/fm/modules/mf-registry"},
+  {provide: 'mod_name', useValue: "registry"}
+]
+
+export const IMPORTS_CONF = [
+  BrowserModule,
+  RouterModule.forChild(routes),
+  FormsModule,
+]
+
 
 @NgModule({
   declarations: [],
@@ -7,5 +29,8 @@ import { IMPORTS_CONF, PROVIDERS_CONF } from "./app.module";
     ...IMPORTS_CONF
   ],
   providers: [...PROVIDERS_CONF],
+  bootstrap: [BootstrapComponent],
 })
 export class RemoteEntryModule {}
+
+
